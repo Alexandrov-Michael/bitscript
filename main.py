85fd81feff29a4d882699d0ed112f97ec2d01d52
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-10 17:26:15 +0300

    w

--- main.py
+++ main.py
@@ -10,15 +10,26 @@ pair = "ZILBTC"
 
 client = Client(api_key, api_secret)
 
+for i in range(1000):
+	depth = client.get_order_book(symbol=pair)
+	print depth["bids"][0]
+	print depth["asks"][0]
+	print " "
+
+
+
+
 # candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
 # print candles
 
 
-def listsum(numList):
-    theSum = 0
-    for i in numList:
-        theSum = theSum + i
-    return theSum
+# def listsum(numList):
+#     theSum = 0
+#     for i in numList:
+#         theSum = theSum + i
+#     return theSum
+
+
 
 # def CheckSumList(value, listCheck, listItems):
 # 	if len(listCheck) == listItems:
@@ -29,39 +40,39 @@ def listsum(numList):
 # 	return listCheck
 
 
-changeLine = []
-changeLine2 = []
+# changeLine = []
+# changeLine2 = []
 
 
-for i in range(1000):
-	buyTrade = 0
-	sellTrade = 0
-	trades = client.get_recent_trades(symbol=pair, limit=30)
-	lastTrade = trades[-1]
-	lastPrice = str(lastTrade["price"])
-	lastQ = str(lastTrade["qty"])
-	for x in trades:
-		if x["isBuyerMaker"]:
-			sellTrade = sellTrade + float(x["qty"])
-		else:
-			buyTrade = buyTrade + float(x["qty"])
+# for i in range(1000):
+# 	buyTrade = 0
+# 	sellTrade = 0
+# 	trades = client.get_recent_trades(symbol=pair, limit=30)
+# 	lastTrade = trades[-1]
+# 	lastPrice = str(lastTrade["price"])
+# 	lastQ = str(lastTrade["qty"])
+# 	for x in trades:
+# 		if x["isBuyerMaker"]:
+# 			sellTrade = sellTrade + float(x["qty"])
+# 		else:
+# 			buyTrade = buyTrade + float(x["qty"])
 
 
 	
-	stakan = buyTrade - sellTrade
-	if len(changeLine) == 5:
-		summlist = listsum(changeLine)
-		if len(changeLine2) ==3:
-			summlist2 = listsum(changeLine2)
-			changeLine2 = []
-			print "Движение: %s" %(summlist2)
-		changeLine2.append(summlist)
-		changeLine = []
-	changeLine.append(stakan)
+# 	stakan = buyTrade - sellTrade
+# 	if len(changeLine) == 5:
+# 		summlist = listsum(changeLine)
+# 		if len(changeLine2) ==3:
+# 			summlist2 = listsum(changeLine2)
+# 			changeLine2 = []
+# 			print "Движение: %s" %(summlist2)
+# 		changeLine2.append(summlist)
+# 		changeLine = []
+# 	changeLine.append(stakan)
 
 
 
-	# print stakan
+# 	# print stakan
 	
 
 
