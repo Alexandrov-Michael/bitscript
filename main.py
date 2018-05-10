b8b966c7ae1d07713b3eab50c90771f4b2e1c926
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-10 11:43:49 +0300

    w

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 import * from keys
+from binance.client import Client
 
 pair = "BNBBTC"
 print api_key 
 
 
 
-from binance.client import Client
-client = Client(api_key, api_secret)
 
-candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
-candles
+# client = Client(api_key, api_secret)
+
+# candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
+# candles
