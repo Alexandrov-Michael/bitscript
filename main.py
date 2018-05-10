e2d7cc64a92d7f58203566a1004fad3b57ba6a0a
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-10 12:43:46 +0300

    w

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ client = Client(api_key, api_secret)
 # candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
 # print candles
 
-buyTrade = 0
-sellTrade = 0
 
-trades = client.get_recent_trades(symbol=pair, limit=30)
-for x in trades:
-	if x["isBuyerMaker"]:
-		buyTrade = buyTrade + float(x["qty"])
-	else:
-		sellTrade = sellTrade + float(x["qty"])
 
-print "buyTrade summ: %s" % (buyTrade) 
-print " "
-print "sellTrade summ: %s" % (sellTrade) 
+
+for i in range(10):
+	buyTrade = 0
+	sellTrade = 0
+	trades = client.get_recent_trades(symbol=pair, limit=30)
+	for x in trades:
+		if x["isBuyerMaker"]:
+			buyTrade = buyTrade + float(x["qty"])
+		else:
+			sellTrade = sellTrade + float(x["qty"])
+
+	print "buyTrade summ: %s" % (buyTrade) 
+	print "sellTrade summ: %s" % (sellTrade) 
+	print " "
