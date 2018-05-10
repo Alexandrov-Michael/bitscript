2744f5028ed02c2c9c7a7136abe213262d77ed75
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-10 12:15:41 +0300

    w

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from keys import *
 from binance.client import Client
 
 pair = "BNBBTC"
-print api_key 
 
 
 
 
-# client = Client(api_key, api_secret)
 
-# candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
-# candles
+client = Client(api_key, api_secret)
+
+candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
+candles
