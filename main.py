d56e693867ebb880cfbe8d28cf26219d7f9be5fb
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-10 12:33:28 +0300

    w

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from keys import *
 from binance.client import Client
 
-pair = "BNBBTC"
+pair = "ZILBTC"
 
 
 
@@ -10,5 +10,19 @@ pair = "BNBBTC"
 
 client = Client(api_key, api_secret)
 
-candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
-print candles
+# candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_5MINUTE)
+# print candles
+
+buyTrade = 0
+sellTrade = 0
+
+trades = client.get_recent_trades(symbol=pair, limit=30)
+for x in trades:
+	if x.["isBuyerMaker"]:
+		buyTrade = buyTrade + x.["isBuyerMaker"]
+	else
+		sellTrade = sellTrade + x.["isBuyerMaker"]
+
+print "buyTrade summ: %s" % (buyTrade) 
+print " "
+print "sellTrade summ: %s" % (sellTrade) 
