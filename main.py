b9ed90783741742c6d7fe5bbcaba59f24e842677
Alexandrov-Michael <mikle.alex@gmail.com>
2018-05-11 11:28:04 +0300

    w

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from binance.client import Client
 # from binance.websockets import BinanceSocketManager
 
 pair = "ZILBTC"
-
 client = Client(api_key, api_secret)
 # bidLast = 0
 # askLast = 0
@@ -31,77 +30,56 @@ client = Client(api_key, api_secret)
 
 candles = client.get_klines(symbol=pair, interval=Client.KLINE_INTERVAL_1MINUTE, limit=10)
 
-print candles[0]
+# import numpy as np
+# import pandas as pd
 
-import numpy as np
-import pandas as pd
+# nda1 = np.array(candles)
+# df = pd.DataFrame(nda1)
 
-nda1 = np.array(candles)
+# def bollinger_strat(df, window, no_of_std):
+#     rolling_mean = df[4].rolling(window).mean()
+#     rolling_std = df[4].rolling(window).std()
 
-df = pd.DataFrame(nda1)
-
-
-def bollinger_strat(df, window, no_of_std):
-    rolling_mean = df[4].rolling(window).mean()
-    rolling_std = df[4].rolling(window).std()
-
-    df['Bollinger High'] = rolling_mean + (rolling_std * no_of_std)
-    df['Bollinger Low'] = rolling_mean - (rolling_std * no_of_std)     
-
-print bollinger_strat(df,5,2)
+#     df['Bollinger High'] = rolling_mean + (rolling_std * no_of_std)
+#     df['Bollinger Low'] = rolling_mean - (rolling_std * no_of_std)    
 
 
+# df['Bollinger High'] = rolling_mean + (rolling_std * 2)
+# df['Bollinger Low'] = rolling_mean - (rolling_std * 2)    
+# print bollinger_strat(df,5,2)
+
+# changeLine = []
 
 # def listsum(numList):
 #     theSum = 0
 #     for i in numList:
 #         theSum = theSum + i
 #     return theSum
+stakanLast = 0
+
+for i in range(1000):
+	buyTrade = 0
+	sellTrade = 0
+	trades = client.get_recent_trades(symbol=pair, limit=30)
+	lastTrade = trades[-1]
+	lastPrice = str(lastTrade["price"])
+	lastQ = str(lastTrade["qty"])
+	for x in trades:
+		if x["isBuyerMaker"]:
+			sellTrade += float(x["qty"])
+		else:
+			buyTrade += float(x["qty"])
+	stakan = buyTrade - sellTrade
+	stakanChange = stakanLast - stakan
+	print "Стакан: %s   Изменение стакана: %s" % (str(stakan), str(stakanChange))
+	stakanLast = stakan
 
 
 
-# def CheckSumList(value, listCheck, listItems):
-# 	if len(listCheck) == listItems:
-# 		summListCheck = listsum(listCheck)
-# 		listCheck = []
-# 		print "Движение: %s" % (summListCheck)
-# 	listCheck.append(value)
-# 	return listCheck
-
-
-# changeLine = []
-# changeLine2 = []
-
-
-# for i in range(1000):
-# 	buyTrade = 0
-# 	sellTrade = 0
-# 	trades = client.get_recent_trades(symbol=pair, limit=30)
-# 	lastTrade = trades[-1]
-# 	lastPrice = str(lastTrade["price"])
-# 	lastQ = str(lastTrade["qty"])
-# 	for x in trades:
-# 		if x["isBuyerMaker"]:
-# 			sellTrade = sellTrade + float(x["qty"])
-# 		else:
-# 			buyTrade = buyTrade + float(x["qty"])
-
-
-	
-# 	stakan = buyTrade - sellTrade
-# 	if len(changeLine) == 5:
-# 		summlist = listsum(changeLine)
-# 		if len(changeLine2) ==3:
-# 			summlist2 = listsum(changeLine2)
-# 			changeLine2 = []
-# 			print "Движение: %s" %(summlist2)
-# 		changeLine2.append(summlist)
-# 		changeLine = []
-# 	changeLine.append(stakan)
-
-
-
-# 	# print stakan
+	# if len(changeLine) == 3:
+	# 	summlist = listsum(changeLine)
+	# 	changeLine = []
+	# changeLine.append(stakan)
 	
 
 
